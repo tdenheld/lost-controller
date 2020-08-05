63830f832b515d48acf5862f0667cd6d9ebd6069
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2020-08-05 10:53:00 +0200

    update sass $vars

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 	<div id="app">
 		<div id="nav">
 			<router-link to="/">Home</router-link>|
-			<router-link to="/about">About</router-link>
+			<router-link to="/scene">Scene</router-link>
 		</div>
 		<router-view />
 	</div>
 </template>
 
 <style lang="scss">
-
+	@import '/assets/sass/style.scss';
 </style>
