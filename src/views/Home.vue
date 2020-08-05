b159f19b9c8461e4362dde9243c164fdadd83982
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2020-08-05 10:41:52 +0200

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
 	<div class="home">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<h1>Hoi</h1>
 	</div>
 </template>
-
-<script>
-	// @ is an alias to /src
-	import HelloWorld from '@/components/HelloWorld.vue';
-
-	export default {
-		name: 'Home',
-		components: {
-			HelloWorld,
-		},
-	};
-</script>
