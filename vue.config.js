2cca9a6795f8ee01909bd7122a22c4122151a55e
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2020-08-06 09:07:54 +0200

    add meta data in vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
     chainWebpack: config => {
         config.plugins.delete('prefetch');
         config.plugins.delete('preload');
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'freek fabricius — lost my controller';
+                args[0].meta = {
+                    description: 'Album cover animation for EP of Freek Fabricius.'
+                };
+                return args;
+            });
     }
 };
