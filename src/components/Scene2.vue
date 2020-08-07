99143140d44bc28787db4b6685c195946328d3ea
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2020-08-07 16:26:25 +0200

    scene 2 edit

--- src/components/Scene2.vue
+++ src/components/Scene2.vue
@@ -1,6 +1,15 @@
 <template>
 	<div>
 		<div>
+			<img
+				v-for="index in 16"
+				:key="'lost' + index"
+				src="@/assets/img/lost.png"
+				alt="lost"
+				class="img s-lost"
+			/>
+		</div>
+		<div>
 			<img v-for="index in 36" :key="'my' + index" src="@/assets/img/my.png" alt="my" class="img s-my" />
 		</div>
 		<div>
@@ -20,40 +29,48 @@
 	export default {
 		methods: {
 			timeline() {
-				const tl = gsap
-					.timeline({
+				const tl = gsap.timeline({
 						defaults: {
 							opacity: 1,
 							duration: 0.4,
 							ease: 'power4.out',
 						},
 					})
-					.fromTo(
-						'.s-controller',
-						{
-							opacity: 0,
-							y: 30,
-						},
-						{
-							y: 0,
-							stagger: 0.02,
-							yoyo: true,
-							repeat: -1,
-						}
-					)
-					.fromTo(
-						'.s-my',
-						{
-							opacity: 0,
-							x: -30,
-						},
-						{
-							stagger: 0.01,
-							yoyo: true,
-							repeat: -1,
-							x: 0,
-						}
-					);
+					.fromTo('.s-controller', {
+                        opacity: 0,
+                        y: 30,
+                    },{
+                        y: 0,
+                        stagger: 0.02,
+                        yoyo: true,
+                        repeat: -1,
+                    })
+					.fromTo('.s-my', {
+                        opacity: 0,
+                        x: 'random(-100, 100)',
+                    },{
+                        stagger: 0.01,
+                        yoyo: true,
+                        repeat: -1,
+                        x: 0,
+                    });
+
+				const lost = () => { 
+                    gsap.fromTo('.s-lost', {
+                        opacity: 0,
+                    },{
+                        opacity: 1,
+                        duration: 0.4,
+                        ease: 'power4.out',
+                        stagger: 0.05,
+                        x: 'random(-700, 700)',
+                        y: 'random(-500, 500)',
+                        onComplete() {
+                            lost();
+                        },
+                    });
+                }
+                lost();
 			},
 		},
 		mounted() {
@@ -68,8 +85,8 @@
 	.s-controller {
 		@for $i from 0 through 64 {
 			&:nth-child(#{$i}) {
-				top: 10% + ($i * 2.5);
-				left: 10% + ($i * 2);
+				top: 14% + ($i * 2.5);
+				left: 3% + ($i * 2);
 			}
 		}
 	}
@@ -78,8 +95,13 @@
 		@for $i from 0 through 64 {
 			&:nth-child(#{$i}) {
 				top: -10% + ($i * 3);
-				left: 30%;
+				left: 80%;
 			}
 		}
+	}
+
+	.s-lost {
+		top: 50%;
+		left: 50%;
 	}
 </style>
