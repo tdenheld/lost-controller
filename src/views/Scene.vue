51ea11302eca57e17eb91227c650137c00bb8131
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2020-08-07 22:23:35 +0200

    knipper

--- src/views/Scene.vue
+++ src/views/Scene.vue
@@ -2,21 +2,21 @@
 	<div class="pb-8">
 		<h1 class="f40 mb-2">{{ $route.params.id | replace('-', ' ') }}</h1>
 		<div class="s-frame">
-            <component v-bind:is="scene" class="full-absolute"></component>
-        </div>
+			<component v-bind:is="scene" class="full-absolute"></component>
+		</div>
 	</div>
 </template>
 
 <script>
-    import Scene1 from '@/components/Scene1.vue';
-    import Scene2 from '@/components/Scene2.vue';
-    import Scene3 from '@/components/Scene3.vue';
+	import Scene1 from '@/components/Scene1.vue';
+	import Scene2 from '@/components/Scene2.vue';
+	import Scene3 from '@/components/Scene3.vue';
 
 	export default {
 		components: {
-            Scene1,
-            Scene2,
-            Scene3
+			Scene1,
+			Scene2,
+			Scene3,
 		},
 		computed: {
 			scene() {
@@ -31,10 +31,41 @@
 <style lang="scss" scoped>
 	@import '@/assets/sass/_settings-tools.scss';
 
+	@keyframes bg-color {
+		0% {
+			background-color: $white;
+		}
+
+		5% {
+			background-color: $dark;
+        }
+        
+        10% {
+			background-color: $white;
+        }
+        
+        15% {
+			background-color: $dark;
+		}
+
+		80% {
+			background-color: $white;
+		}
+
+		90% {
+			background-color: $dark;
+		}
+
+		100% {
+			background-color: $white;
+		}
+	}
+
 	.s-frame {
-        position: relative;
-        padding-bottom: 56.25%;
-        overflow: hidden;
-        border: 1px solid $dark;
+		position: relative;
+		padding-bottom: 56.25%;
+		overflow: hidden;
+		border: 1px solid $dark;
+		animation: bg-color 500ms $ease-out infinite both;
 	}
 </style>
